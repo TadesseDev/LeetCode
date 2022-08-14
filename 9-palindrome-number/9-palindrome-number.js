edfb6a2141b67487f73544c8f65587c36696fa63
Tadesse Alemayehu <tadalem26@gmail.com>
2022-08-14 12:46:40 +0300

    Time: 338 ms (22.61%), Space: 51 MB (64.39%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -4,9 +4,11 @@
  */
 var isPalindrome = function(x) {
     let arr=String(x);
-    for(let i=0,j=arr.length-1;i<j;i++,j--){
+    let i=0,j=arr.length-1;
+    while(i<j){
         if(arr.at(i)!=arr.at(j))
             return false;
+        i++,j--;
     }
   return true
 };
