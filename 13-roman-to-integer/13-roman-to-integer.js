4f2c96aa97672eb1d037ba4e0e2b58b2733dc8b4
Tadesse Alemayehu <tadalem26@gmail.com>
2022-08-14 14:55:12 +0300

    Time: 120 ms (97.69%), Space: 47.5 MB (50.96%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -5,20 +5,17 @@
 var romanToInt = function(s) {
     let map={'IV': '4', 'IX': "9", 'XL': "40", 'XC': "90", 'CD': "400", 'CM': "900",
             'I': '1','V': '5','X': '10','L': '50','C': '100','D': '500','M': '1000'}
-    function convert(r){
-        if(!r.length)
-            return 0;
-        res=0;
-        if(r.length>1&&map[r.substring(0,2)]){
-            res+=Number(map[r.substring(0,2)]);
-            r=r.substring(2)
+        let number=0;
+    while(s.length>0){
+        if(map[s.substring(0,2)]){
+            number+=Number(map[s.substring(0,2)])
+            s=s.substring(2)
         }
         else{
-            res+=Number(map[r.substring(0,1)]);
-            r=r.substring(1)
-        }
-        console.log(res)
-        return res+=Number(convert(r));
+            number+=Number(map[s.substring(0,1)])
+            s=s.substring(1)
+        }   
     }
-    return convert(s)
-};
+    return number;
+}
+   
