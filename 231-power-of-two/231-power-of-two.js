55f96305028d6e28317a982d92296a8933c64d4d
Tadesse Alemayehu <tadalem26@gmail.com>
2022-08-24 18:11:29 +0300

    Time: 73 ms (95.43%), Space: 42.1 MB (99.82%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+        if(n<1)
+        return false
+while(n>1){
+    if(n%2!=0)
+        return false;
+    n=n/2
+}
+    return true;
+};
