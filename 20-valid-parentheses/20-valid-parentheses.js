d5105c1d8e751e8785950bac937208d8f34e4189
Tadesse Alemayehu <tadalem26@gmail.com>
2022-08-26 18:40:17 +0300

    Time: 160 ms (6.84%), Space: 45.4 MB (9.69%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let pair={')':'(',']':'[','}':'{'}
+    let store=[];
+for (let i=0;i<s.length;i++){
+    let ele=s.charAt(i);
+    if(pair[ele]){
+    console.log(i)
+        if(store.length<1)
+            return false;
+        if(store[store.length-1]!=pair[ele])
+            return false;
+        store.pop();
+    }
+    else
+        store.push(ele)
+}
+    return store.length==0
+};
