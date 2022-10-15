3cd2cc0648be2fd96c6cddc01b3b73ef6919d0e8
Tadesse Alemayehu <tadalem26@gmail.com>
2022-10-15 20:57:55 +0300

    Time: 549 ms (24.90%), Space: 211 MB (95.56%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rb
@@ -0,0 +1,19 @@
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+    start=0
+    end_ele=0
+    result=''
+    for i in 0..s.length-1
+        new_str=s[start, end_ele-start]||''
+        while end_ele<s.length && !new_str.include?(s[end_ele])
+            new_str.insert(-1,s[end_ele])
+            end_ele+=1
+        end
+        puts start
+        result = new_str if new_str.length > result.length
+        i=end_ele
+        start=s.index(s[end_ele], start)+1 if s[end_ele]
+    end
+    result.length
+end
