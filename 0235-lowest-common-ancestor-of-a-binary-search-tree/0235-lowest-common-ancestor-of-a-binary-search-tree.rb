88ff9e3a10fc73c72e67ad3629b955cca749e58d
Tadesse Alemayehu <tadalem26@gmail.com>
2022-11-27 14:57:02 +0300

    Time: 118 ms (86.82%), Space: 212.5 MB (24.03%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.rb
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.rb
@@ -16,18 +16,13 @@ def lowest_common_ancestor(root, p, q)
 
     def lowest(root, p, q)
          # p root.val
-        ret=0
-         if root.left 
-            res = lowest(root.left, p, q)
-            return res if res.class == TreeNode
-            ret+=res
-         end
-         if root.right
-            res = lowest(root.right, p, q)
-            return res if res.class == TreeNode
-            ret+=res
-         end
-        
+        ret=0 
+        left, right=0,0
+            left = lowest(root.left, p, q) if root.left 
+            return left if left.class == TreeNode
+            right = lowest(root.right, p, q) if root.right
+            return right if right.class == TreeNode
+        ret+=left+right
         ret+=1 if (root==p || root==q)
         return root if ret > 1
         ret
