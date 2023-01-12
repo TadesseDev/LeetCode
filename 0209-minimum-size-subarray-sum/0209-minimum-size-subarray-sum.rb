2eb0022724ec7256cffb1f79b9ce17ce950cda28
Tadesse Alemayehu <tadalem26@gmail.com>
2023-01-12 14:52:48 +0300

    Time: 94 ms (100.00%), Space: 213.9 MB (75.00%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.rb
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.rb
@@ -2,16 +2,15 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def min_sub_array_len(target, nums)
-    window,sum,i,minLength=[0,0],0,0,0
+    window,sum,i,minLength=0,0,0,0
     while i<nums.length
         sum+=nums[i]
-        window[1]=i
         if sum>=target 
-            windowSize=window[1]-window[0]+1;
+            windowSize=i-window+1;
             minLength=windowSize if windowSize<minLength || minLength==0
-            sum-=nums[window[0]]
+            sum-=nums[window]
             sum-=nums[i]
-            window[0]+=1
+            window+=1
             next;
         end
         i+=1
