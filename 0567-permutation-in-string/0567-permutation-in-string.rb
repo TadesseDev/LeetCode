b4d00a3b9d300543724d75d1876e90b54a97977f
Tadesse Dubale <tadalem26@gmail.com>
2023-03-04 21:44:54 +0300

    Time: 148 ms (60.00%), Space: 218 MB (5.45%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.rb
+++ 0567-permutation-in-string/0567-permutation-in-string.rb
@@ -3,7 +3,8 @@
 # @return {Boolean}
 def check_inclusion(s1, s2)
     hash={}
-    
+    # count=s1.length
+    # @s2=s2
     return false if s1.length > s2.length
     # store s1 chars into a hash
     s1.each_char do |char|
@@ -13,6 +14,34 @@ def check_inclusion(s1, s2)
             hash[char]=1
         end
     end
+    
+#     def recursive_check(position, hash)
+#         if hash[@s2[position]] && hash[@s2[position]]>0
+#             @count-=1
+#             return true if @count==0
+#             hash[@s2[position]]-=1
+#             ret=recursive_check(position+1, hash)
+#             @count+=1
+#             hash[@s2[position]]+=1 if hash[@s2[position]]
+#             return ret
+#         else 
+#             return position
+#         end
+#     end
+    
+#     i=0
+#     while i<s2.length
+#          if hash[s2[i]]
+#             ret= recursive_check(i, {**hash})
+#              return true if ret.class !=Integer
+#              i=ret
+#          else
+#             i+=1
+#          end
+#     end
+#     false
+    
+    
     # Check for permutuation with O(n)
     temp_hash={**hash}
     window=[]
