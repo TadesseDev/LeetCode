b29f34378cb6373b3ac31b62b24e38e62a1ef3dd
Tadesse Dubale <tadalem26@gmail.com>
2023-03-05 11:15:35 +0300

    Time: 253 ms (8.33%), Space: 212.5 MB (100.00%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.rb
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.rb
@@ -3,18 +3,18 @@
 # @param {Integer} x
 # @return {Integer[]}
 def find_closest_elements(arr, k, x)
-    window=arr.slice(0,k)
+    j=k-1
     i=k
     while i<arr.length
-        if (x-arr[i]).abs <= (x-window[0]).abs
-            if (x-arr[i]).abs < (x-window[0]).abs
-                window.shift
-                window.push(arr[i])
+        if (x-arr[i]).abs <= (x-arr[(j-k)+1]).abs
+            if (x-arr[i]).abs < (x-arr[(j-k)+1]).abs
+                p arr[i],arr[j-k]
+                j=i
             end
         else 
-            return window
+            return arr.slice(j-k+1,k) 
         end
         i+=1
     end
-    window
+    arr.slice(j-k+1,k) 
 end
