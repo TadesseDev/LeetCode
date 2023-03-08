012a4e783c40b5664c77118f8620618150ca6d9f
Tadesse Dubale <tadalem26@gmail.com>
2023-03-08 15:44:30 +0300

    Time: 76 ms (81.92%), Space: 211 MB (85.38%) - LeetHub

--- 0066-plus-one/0066-plus-one.rb
+++ 0066-plus-one/0066-plus-one.rb
@@ -1,18 +1,17 @@
 # @param {Integer[]} digits
 # @return {Integer[]}
 def plus_one(digits)
-    new_arr=[*digits]
-    i=new_arr.length-1
+    i=digits.length-1
     while i>=0 
-        num=1+new_arr[i]
+        num=1+digits[i]
         if num==10
             num=0
-            new_arr[i]=num
+            digits[i]=num
         else 
-            new_arr[i]=num
-            return new_arr
+            digits[i]=num
+            return digits
         end
         i-=1
     end
-    new_arr.unshift(1) 
+    digits.unshift(1) 
 end
