3fb9e3861d33c02f966132e98209ed83428c6d5d
Tadesse Dubale <tadalem26@gmail.com>
2023-03-22 16:07:45 +0300

    Time: 793 ms (75.00%), Space: 211.6 MB (100.00%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.rb
@@ -0,0 +1,19 @@
+# @param {String} s
+# @param {String} t
+# @param {Integer} max_cost
+# @return {Integer}
+def equal_substring(s, t, max_cost)
+     length=i=j=0
+    
+    while i<s.length 
+        max_cost-=(s[i].ord-t[i].ord).abs
+        if max_cost<0
+            max_cost+=(s[j].ord-t[j].ord).abs
+            j+=1
+        else
+            length=i-j+1 
+        end
+        i+=1
+    end
+    length
+end
