0c1a78149431826811de9a30720fc4bfc1ef2897
Tadesse Dubale <tadalem26@gmail.com>
2023-05-09 16:12:47 +0300

    Time: 70 ms (11.55%), Space: 44.3 MB (5.22%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start_i=0,end_i=nums.length-1
+    
+    while(start_i<=end_i){
+        let mid=Math.floor(((start_i+end_i)/2))
+        if(nums[mid]==target )
+            return mid;
+        
+        if(nums[start_i] <= nums[mid]){
+            if(target >= nums[start_i] && target<nums[mid] ){
+                end_i=mid-1 
+            }
+            else{
+                start_i=mid+1 
+            }
+        }
+        else{
+            if(target>nums[mid] && target<=nums[end_i]){
+                start_i=mid+1
+            }
+            else{
+                end_i=mid-1
+            }
+        }
+        console.log(mid)
+    }
+        
+    return  -1; 
+};
