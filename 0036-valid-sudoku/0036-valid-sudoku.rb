999b9c2951f8690c09f739ceb26a932112f48e3a
Tadesse Dubale <tadalem26@gmail.com>
2023-05-15 16:44:27 +0300

    Time: 108 ms (42.06%), Space: 211 MB (47.66%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.rb
@@ -0,0 +1,45 @@
+# @param {Character[][]} board
+# @return {Boolean}
+def is_valid_sudoku(board)
+    def check_sub_box(cel, arr)
+        x,y=*cel
+        square_hash={}
+        for j in x..x+2
+            for k in y..y+2
+                # p [j,k]
+                return false if square_hash[arr[j][k]] 
+                square_hash[arr[j][k]]=true if arr[j][k] !='.'
+            end
+        end
+        # p "end"
+        true
+    end
+    def taceCell(cel, arr)
+        i,j=*cel
+        left_hash={}
+        right_hash={}
+        for k in 0..8 do 
+            return false if left_hash[arr[i][k]] || right_hash[arr[k][j]]
+            left_hash[arr[i][k]] = true if arr[i][k] !='.'
+            right_hash[arr[k][j]] = true if arr[k][j] !='.'
+        end
+        # p left_hash
+        return true;
+    end
+    
+    j=0
+    while j<9
+        k=0
+        while k<9
+            # p m
+            return false unless check_sub_box([j,k], board)
+
+            k+=3
+        end
+        j+=3
+    end
+    for m in 0..8 do
+        return false if !taceCell([m,m], board)
+    end
+    return true;
+end
