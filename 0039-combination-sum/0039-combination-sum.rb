5ad0f0238f4a12e6de3be880a7910e87563e1b4c
Tadesse Dubale <tadalem26@gmail.com>
2023-05-16 18:04:03 +0300

    Time: 106 ms (73.50%), Space: 211.5 MB (24.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.rb
+++ 0039-combination-sum/0039-combination-sum.rb
@@ -7,11 +7,11 @@ def combination_sum(candidates, target)
     def get_result(candidates, target)
         return [] if candidates.length == 0 || candidates[0]>target
         ele=candidates[0]
-        ret=get_result(candidates.slice(1,candidates.length), target)
+        ret=get_result(candidates[1..candidates.length], target)
         ret.push([[ele], ele])
         @result.push([ele]) if ele == target
         min_sum=ele
-        temp=[*ret]
+        temp=ret
         while min_sum<target
             new_valid_elements=[]
             temp.each do |arr|
@@ -21,7 +21,7 @@ def combination_sum(candidates, target)
                     @result.push(new_res[0]) if new_res[1]==target
                 end
             end
-            temp=[*new_valid_elements]
+            temp=new_valid_elements
             ret.push(*new_valid_elements)
             min_sum+=ele
         end
