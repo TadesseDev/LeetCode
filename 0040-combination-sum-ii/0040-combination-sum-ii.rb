e35d06fbf2e1fba1deb6f9b644635815005aa943
Tadesse Dubale <tadalem26@gmail.com>
2023-05-17 20:15:48 +0300

    Time: 267 ms (8.33%), Space: 213.7 MB (8.33%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.rb
@@ -0,0 +1,37 @@
+# @param {Integer[]} candidates
+# @param {Integer} target
+# @return {Integer[][]}
+def combination_sum2(candidates, target)
+    @result={}
+    def get_combination(candidates, target, permutuation, index)
+        return [] if candidates.length == index 
+        ele=candidates[index]
+        result=[]
+         if ele==target
+            result.push([ele])
+             return result
+         else
+            next_permutuation=[*permutuation]
+            permutuation.each do |arr|
+                new_combination = [[*arr[0]].push(ele), arr[1]+ele]
+                next if @result[new_combination[0]]
+                @result[new_combination[0]]=true
+                if new_combination[1] <= target
+                     if new_combination[1] < target
+                         next_permutuation.push(new_combination)
+                     else
+                         result.push(new_combination[0]) 
+                          
+                     end
+                end
+            end
+            next_permutuation.push([[ele], ele])
+            permutuation=next_permutuation
+        end
+        # p permutuation
+        return get_combination(candidates, target, permutuation, index+1) + result
+        
+    end
+    
+    get_combination(candidates.filter{|ele| ele<=target}.sort, target, [], 0)
+end
