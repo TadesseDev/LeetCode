5895a2f3173079ddc22d3a88a10bbd4c206430fd
Tadesse Dubale <tadalem26@gmail.com>
2023-05-18 16:39:51 +0300

    Time: 131 ms (30.56%), Space: 212.9 MB (13.89%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.rb
+++ 0045-jump-game-ii/0045-jump-game-ii.rb
@@ -1,22 +1,23 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def jump(nums)
-   def get_jump(nums, current_element)
-       return [0] if current_element == nums.length-1
-       
-       ret=get_jump(nums, current_element+1)
-
+   def get_jump(nums, current_element, count)
+       return count+1 if current_element+nums[current_element] >= nums.length-1
        jump=nums[current_element]
-       min=(nums.length-(current_element+jump))-1
-       return ret.unshift(1) if min<=0
-       
-           i=0
-            while i<jump && min>1
-                min=ret[i] if ret[i]<min
-                i+=1
+       max=jump+current_element
+       next_element=current_element+1
+       count+=1
+       (next_element..current_element+jump).each do |i|
+           
+           if nums[i]+i > max
+               next_element=i
+               max=nums[i]+i 
+               p max
+               return count+1 if max >= nums.length-1
            end
-       
-       ret.unshift(min+1)
+       end
+        get_jump(nums, next_element, count)
     end
-    get_jump(nums, 0)[0]
+    return 0 if nums.length<2
+    get_jump(nums, 0, 0)
 end
