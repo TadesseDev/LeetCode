b9932912ed091e624534584800ea4cb4e29a1fbd
Tadesse Dubale <tadalem26@gmail.com>
2023-05-24 12:09:21 +0300

    Time: 97 ms (93.75%), Space: 211 MB (87.50%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.rb
@@ -0,0 +1,29 @@
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute_unique(nums)
+    
+    def unique(nums)
+    return [[nums[0]]] if nums.length<2 / 1
+    
+    i=0
+    store=[]
+    while i<nums.length # 0<3 / 0<2
+        while i<nums.length-1 && nums[i]==nums[i+1] do #true | i=1 / false / false
+            i+=1
+        end
+        cur=nums[i]
+        nex_nums=i>0 ? nums[0..i-1] : [] 
+        nex_nums=nex_nums+nums[i+1..nums.length]
+        # p [nex_nums, i,nums[i+1..nums.length]]
+        res=unique(nex_nums)
+        
+        res.each do |arr|
+            arr.unshift(cur)
+        end
+        store.push(*res)
+        i+=1
+    end
+    store
+end
+    unique(nums.sort)
+end
