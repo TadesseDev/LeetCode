9379d05e31ce2490f4d2a0e3010d8ab59af8c29e
Tadesse Dubale <tadalem26@gmail.com>
2023-05-26 14:24:39 +0300

    Time: 166 ms (22.91%), Space: 224.8 MB (42.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.rb
+++ 0217-contains-duplicate/0217-contains-duplicate.rb
@@ -1,10 +1,10 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def contains_duplicate(nums)
-    # hash=Set.new
-    nums.sort!
-    nums.each_with_index do |n,index|
-        return true if n==nums[index+1]
+    hash=Set.new
+    nums.each do |n|
+        return true if hash.include?(n)
+        hash<<n
     end
     return false
 end
