2fdca85a59f7e284cc97c31b653ca892f1f5861a
Tadesse Dubale <tadalem26@gmail.com>
2023-05-27 16:50:21 +0300

    Time: 211 ms (50.00%), Space: 217.1 MB (47.08%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.rb
@@ -0,0 +1,20 @@
+# @param {String[]} strs
+# @return {String[][]}
+def group_anagrams(strs)
+    sorted=[]
+    hash={}
+    strs.each do |str|
+        sorted.push(str.chars.sort.join())
+    end
+    
+    sorted.each_with_index do |str,index|
+        if hash[str]
+            hash[str].push(strs[index])
+        else
+            hash[str]=[strs[index]]
+        end
+    end
+    result=[]
+    hash.each {|key, val| result.push(val)}
+    p result
+end
