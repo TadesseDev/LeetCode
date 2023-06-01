5ffa0d0c2d9d8b968a180055b7dec20865f00978
Tadesse Dubale <tadalem26@gmail.com>
2023-06-01 17:27:42 +0300

    Time: 262 ms (47.87%), Space: 229.4 MB (36.17%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.rb
@@ -0,0 +1,18 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def longest_consecutive(nums)
+    return 0 if nums.length<1
+    nums.sort!.uniq!
+    count=1
+    max_count=1
+    p nums
+    for i in 1..nums.length-1
+        if nums[i]!=nums[i-1]+1
+            max_count=count if count>max_count
+            count=0
+        end
+        count+=1
+    end
+    max_count=count if count>max_count
+    max_count
+end
