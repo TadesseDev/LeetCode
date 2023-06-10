5f014146b6dd784680543bb5fbcea5a4b6e4d317
Tadesse Dubale <tadalem26@gmail.com>
2023-06-10 13:45:17 +0300

    Time: 53 ms (94.46%), Space: 42.7 MB (37.87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -6,17 +6,14 @@ var isValid = function(s) {
     let hash={'{': '}','[': ']','(': ')'}
     const stack=[]
     for(let char of s){
-        if(hash[char]){
+        if(hash[char])
             stack.push(char)
-        }
-        else{
-            if(hash[stack.pop()]==char){
+        else
+            if(hash[stack.pop()]==char)
                 continue;
-        }
-            else {
+            else
                 return false
-            }
-        }
+
     }
     return stack.length==0
 };
