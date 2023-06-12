73cfb12c3822ba26aa3774c1814d36f51ea093b6
Tadesse Dubale <tadalem26@gmail.com>
2023-06-12 15:50:38 +0300

    Time: 60 ms (55.49%), Space: 41.9 MB (70.91%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let left=0
+    let right=matrix.length-1
+    let mid=0
+    while(left<=right){
+    // console.log(mid, matrix.length)
+        mid=Math.floor((left+right)/2)
+        if(matrix[mid][0]==target){
+            return true
+        }else if(target>matrix[mid][0]){
+            left=mid+1
+        }else{
+            right=mid-1
+        }
+    }
+    
+    // console.log(mid, matrix.length)
+    if(target>matrix[mid][matrix[0].length-1]){
+            mid+=1
+        }else if(target<matrix[mid][0]){
+            mid-=1
+        }
+    if(mid>=matrix.length||mid<0)
+        return false
+    let row=matrix[mid]
+    left=0,right=row.length-1
+    while(left<=right){
+        mid=Math.floor((left+right)/2)
+        if(row[mid]==target){
+            return true
+        }else if(target>row[mid]){
+            left=mid+1
+        }else{
+            right=mid-1
+        }
+    }
+    return false
+};
