eae2f24ccc9d8936ea9d26c98b5777d953fe85ca
Tadesse Dubale <tadalem26@gmail.com>
2023-06-12 15:11:27 +0300

    Time: 71 ms (20.73%), Space: 45.6 MB (7.37%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -11,17 +11,16 @@ var search = function(nums, target) {
     let arr=[...nums]
     let left=0
     let right=arr.length
-    let mid=Math.floor((left+right)/2)//1
     while(left<=right){
+    let mid=Math.floor((left+right)/2)//1
         if(target==arr[mid]){ //arr[4]==-1
             return mid
         }else if(target>arr[mid]){//-1>arr[1] | 
             left=mid+1
-            // mid=Math.ceil((left+right)/2)//1
         }else{
             right=mid-1 // // //
         }
-            mid=Math.floor((left+right)/2)//1
+            // mid=Math.floor((left+right)/2)//1
     }
     return -1
 };
