aaaef2e8650c311811a227445dab1b5f2b632d84
Tadesse Dubale <tadalem26@gmail.com>
2023-06-13 11:12:26 +0300

    Time: 98 ms (43.09%), Space: 44.8 MB (92.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -15,20 +15,19 @@ var lengthOfLongestSubstring = function(s) {
     let hash=new Map([[s[0],0]])
     let max=0
     let l=0,r=1
-    while(r<s.length){ //1<8 //2<8 //3<8
-        if(hash.get(s[r])||hash.get(s[r])===0){ // null||false //null||false //0||0==0
-            // console.log(s[r])
+    while(r<s.length){ 
+        if(hash.get(s[r])||hash.get(s[r])===0){ 
             let temp=l //0
-            l=hash.get(s[r])+1 //1
+            l=hash.get(s[r])+1 
             while(temp<l){
-                hash.set(s[temp],false) // 
-                temp+=1 // 1
+                hash.set(s[temp],false) 
+                temp+=1 
             }
-            // console.log(hash)
+
         }
-        // console.log(l,r)
-        hash.set(s[r],r) // set b // set c
-        if((r-l+1)>max) // true
+
+        hash.set(s[r],r) 
+        if((r-l+1)>max) 
             max=r-l+1
         r+=1
     }
