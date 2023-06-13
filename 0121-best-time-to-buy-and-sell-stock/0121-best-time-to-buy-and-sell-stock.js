d3cae1d7308c338c97cc4f4a862e75283784057c
Tadesse Dubale <tadalem26@gmail.com>
2023-06-13 10:19:54 +0300

    Time: 84 ms (58.25%), Space: 51.1 MB (97.61%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let max=prices[0]  
+  let min=max
+  let l=0, r=1, profit=0;
+  while(r<prices.length){
+      if(prices[r]<min){
+          l=r
+          max=prices[l]  
+          min=prices[l]  
+      }else if(prices[r]>max){
+          max=prices[r]
+          let newProfit=max-min
+          if(newProfit>profit){
+              profit=newProfit
+          }
+      }
+      r+=1
+  }
+    return profit;
+};
