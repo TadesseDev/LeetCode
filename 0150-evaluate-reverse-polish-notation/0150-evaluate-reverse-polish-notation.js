4c206398264385180f146e007490880cca7ebe55
Tadesse Dubale <tadalem26@gmail.com>
2023-06-17 16:35:10 +0300

    Time: 68 ms (71.17%), Space: 45 MB (62.26%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -19,23 +19,15 @@ function operat(a,oprand, b){
 }
 
 var evalRPN = function(tokens) {
-    /*
-        ["4","13","5","/","+"]
-        stack[6
-            fs=
-            ss=
-    */
+
     let stack=[]
     for(let token of tokens){
-        // console.log(token)
         if(Number(token)||Number(token)==0)
             stack.push(Number(token))
         else{
             let secondOperand=stack.pop()
             let firstOperand=stack.pop()
-            // console.log(firstOperand, token, secondOperand)
             stack.push(operat(firstOperand, token, secondOperand))
-            // console.log(stack)
         }
     }
     return stack[0]
