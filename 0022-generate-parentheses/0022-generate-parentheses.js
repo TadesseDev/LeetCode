13a29fc8cae9ed7bbe90ec12d0679f91365eff45
Tadesse Dubale <tadalem26@gmail.com>
2023-06-19 14:23:55 +0300

    Time: 73 ms (16.41%), Space: 43.7 MB (41.23%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let original=n
+    const generator=(n)=>{
+        if(n<=0)
+            return [[0,1,")"]]
+      let res=[...generator(n-1)]
+      let ret=[]
+      res.forEach((ele)=>{
+          if(ele[0]<ele[1])
+          ret.push([ele[0]+1,ele[1],"("+ele[2]])
+          if(ele[1]<original)
+          ret.push([ele[0],ele[1]+1,")"+ele[2]]) 
+      })
+        return ret
+    }
+  let result = generator(n*2-1).map(ele=>ele[2])
+  // console.log(result)
+  return result
+};
