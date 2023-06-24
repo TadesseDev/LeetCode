b15341ae33cc7cc7f15617e5465e7a56fed3cdec
Tadesse Dubale <tadalem26@gmail.com>
2023-06-24 16:36:04 +0300

    Time: 246 ms (10.00%), Space: 95.9 MB (5.07%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
@@ -3,19 +3,10 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
-    /*
-    height=0,1,6,10
-        [2,1,5,6,2,3]
-        stack=
-        index=5
-            val=[1,1]
-            
-    */
     let height=0
     let stack=[[0,heights[0]]]
     for(let index=1;index<heights.length;index++){
         let stackIndex=stack.length-1
-        // console.log(stack.length-1, stack[stackIndex][1])
         while(stackIndex>=0 && heights[index]<stack[stackIndex][1]){ // 1 && 1<2 | 2 && 5<1 /false || 2 && 6<5 || 2 && 2<5 || 2 && 3<2      
             let val=[...stack[stackIndex]]
             stack[stackIndex][1]=heights[index]
@@ -24,7 +15,6 @@ var largestRectangleArea = function(heights) {
                 height=he
             stackIndex-=1
         }
-            // console.log(stack)
         stack.push([index,heights[index]])
     }
     let index = heights.length-1
