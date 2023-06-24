90dec9177bdc23cd6d93a18e67622e6e8701ce1e
Tadesse Dubale <tadalem26@gmail.com>
2023-06-24 16:54:07 +0300

    Time: 106 ms (74.00%), Space: 73.9 MB (34.93%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
@@ -3,19 +3,28 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
+    /*
+    height=0,1,6,10
+        [2,1,5,6,2,3]
+        stack=
+        index=5
+            val=[1,1]
+            
+    */
     let height=0
     let stack=[[0,heights[0]]]
     for(let index=1;index<heights.length;index++){
-        let stackIndex=stack.length-1
-        while(stackIndex>=0 && heights[index]<stack[stackIndex][1]){ // 1 && 1<2 | 2 && 5<1 /false || 2 && 6<5 || 2 && 2<5 || 2 && 3<2      
-            let val=[...stack[stackIndex]]
-            stack[stackIndex][1]=heights[index]
+        let extendIndex=index
+        while(stack.length && heights[index]<stack[stack.length-1][1]){ // 1 && 1<2 | 2 && 5<1 /false || 2 && 6<5 || 2 && 2<5 || 2 && 3<2
+            
+    // console.log(stack)
+            let val=stack.pop()
+            extendIndex=val[0]
             let he=((index-val[0]))*val[1] // 1-0*2 =1 || 4-3* 6 || 4-2*5
             if(he>height)
                 height=he
-            stackIndex-=1
         }
-        stack.push([index,heights[index]])
+        stack.push([extendIndex,heights[index]])
     }
     let index = heights.length-1
     while(stack.length){
