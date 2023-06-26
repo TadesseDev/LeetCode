ceef620729b9cb96d9c7058d25c4c954b8fca940
Tadesse Dubale <tadalem26@gmail.com>
2023-06-26 20:25:22 +0300

    Time: 112 ms (42.59%), Space: 46.5 MB (83.99%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -8,14 +8,14 @@ var findMedianSortedArrays = function(nums1, nums2) {
     let i=0,j=0
     while(i<nums1.length){
         while(j<nums2.length&&nums2[j]<=nums1[i]){
-            merged.push(nums2[j])
+            nums1.splice(i,0,nums2[j])
             j+=1
+            i+=1
         }
-        merged.push(nums1[i])
         i+=1
     }
-    merged.push(...nums2.slice(j, nums2.length))
-    // console.log(merged)
-    let len=merged.length-1
-    return (merged[Math.floor(len/2)]+merged[Math.ceil(len/2)])/2
+    nums1.push(...nums2.slice(j, nums2.length))
+    // console.log(nums1)
+    let len=nums1.length-1
+    return (nums1[Math.floor(len/2)]+nums1[Math.ceil(len/2)])/2
 };
