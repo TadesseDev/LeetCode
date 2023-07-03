93bddea6ff6c7d349279f159956877654704e98e
Tadesse Dubale <tadalem26@gmail.com>
2023-07-03 23:42:45 +0300

    Time: 2772 ms (11.05%), Space: 87.5 MB (87.38%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    let res=[]
+    let j=0,i=k, max=nums[0]
+    while(j<i){
+        if(nums[j]>max)
+            max=nums[j]
+        j+=1
+    }
+    j=0
+    res.push(max)
+    while(i<nums.length){
+        if(nums[i]>max){
+            max=nums[i]
+        }else if(nums[j]==max){
+            let temp=j+1, newMax=nums[temp]
+            while(temp<=i){
+                // console.log(newMax)
+                if(newMax<nums[temp])
+                    newMax=nums[temp]
+                temp+=1
+            }
+            max=newMax
+        }
+        res.push(max)
+        j+=1
+        i+=1
+    }
+    return res
+};
