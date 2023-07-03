3495850a8180cc8f0125752c16322c1a2814a22c
Tadesse Dubale <tadalem26@gmail.com>
2023-07-03 23:50:40 +0300

    Time: 5310 ms (6.73%), Space: 87.1 MB (88.94%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.js
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -5,27 +5,27 @@
  */
 var maxSlidingWindow = function(nums, k) {
     let res=[]
-    let j=0,i=k, max=nums[0]
+    let j=0,i=k, max=0
     while(j<i){
-        if(nums[j]>max)
-            max=nums[j]
+        if(nums[j]>nums[max])
+            max=j
         j+=1
     }
     j=0
-    res.push(max)
+    res.push(nums[max])
     while(i<nums.length){
-        if(nums[i]>max){
-            max=nums[i]
-        }else if(nums[j]==max){
-            let temp=j+1, newMax=nums[temp]
-            while(temp<=i&&newMax<max){
-                if(newMax<nums[temp])
-                    newMax=nums[temp]
+        if(nums[i]>=nums[max]){
+            max=i
+        }else if(j==max){
+            let temp=j+1, newMax=temp
+            while(temp<=i){
+                if(nums[newMax]<nums[temp])
+                    newMax=temp
                 temp+=1
             }
             max=newMax
         }
-        res.push(max)
+        res.push(nums[max])
         j+=1
         i+=1
     }
