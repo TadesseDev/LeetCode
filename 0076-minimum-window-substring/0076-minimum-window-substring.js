afb605e7059ef9ebd8942b4ccf0068e1e2dcbcab
Tadesse Dubale <tadalem26@gmail.com>
2023-07-03 21:44:56 +0300

    Time: 91 ms (81.70%), Space: 46.8 MB (53.23%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -7,32 +7,27 @@ var minWindow = function(s, t) {
     let hash={}
     let win=null
     t.split("").forEach(ch=>{
-        hash[ch]=hash[ch]? [hash[ch][0]+1, hash[ch][1]+1] : [1,1]
-        // hash[ch][1]=hash[ch][0]
+        hash[ch]=hash[ch]? hash[ch]+1 : 1
     });
     let j=0,i=0,sum=Object.keys(hash).length
     while(i<s.length){
         let ch=s[i]
-        if(typeof hash[ch]== 'object' ){
-            hash[ch][0]-=1
-            sum=hash[ch][0]==0 ? sum-1 : sum
-        // console.log(sum, i, hash)
+        if(typeof hash[ch]== 'number' ){
+            hash[ch]-=1
+            sum=hash[ch]==0 ? sum-1 : sum
         }
         
         if(sum==0){
-            // console.log(win, s.slice(j,i+1) )
-            // if(win)
             if(!win||win.length>s.slice(j,i+1).length)
                 win=s.slice(j,i+1)
             while(j<=i){
                 ch=s[j]
-                     if(typeof hash[ch]=='object'){
-                        hash[ch][0]+=1
-                        sum=hash[ch][0]>0 ? sum+1 : sum
+                     if(typeof hash[ch]=='number'){
+                        hash[ch]+=1
+                        sum=hash[ch]>0 ? sum+1 : sum
                     } 
-                // console.log(sum, s.charAt(j), hash[ch], hash)
                 if(sum>1){ 
-                    hash[ch][0]-=1
+                    hash[ch]-=1
                     sum-=1
                     break;
                 }else if(sum==0&&win.length>s.slice(j+1,i+1).length){
