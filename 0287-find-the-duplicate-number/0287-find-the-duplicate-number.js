1978c86ab17db809a3b79abd26f57e6ae1acefc1
Tadesse Dubale <tadalem26@gmail.com>
2023-07-16 14:29:45 +0300

    Time: 62 ms (99.40%), Space: 49.2 MB (97.27%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -3,11 +3,7 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    /*
-        [1,3,4,2,2]
-        i=3=2
-        j=2=2
-    */
+
     let j=nums[nums[nums[0]]],i=nums[nums[0]];
     while(i!=j){
         i=nums[i]
@@ -15,14 +11,10 @@ var findDuplicate = function(nums) {
     }
     j=nums[0]
     while(i!=j){
-    // console.log(i,j)
+ 
         i=nums[i]
         j=nums[j]
     }
-    /*
-       [1,3,4,2,2]
-        i=2=4=2
-        j=1=3=2
-    */
+
     return i
 };
