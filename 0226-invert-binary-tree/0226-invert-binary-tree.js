f7924b670e0b46d485f2b2ab5e0b1150a0657d59
Tadesse Dubale <tadalem26@gmail.com>
2023-07-20 17:47:40 +0300

    Time: 66 ms (20.65%), Space: 42.4 MB (32.39%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -13,13 +13,15 @@
 var invertTree = function(root) {
     if(!root)
         return null
+    
+    let temp=root.right
+    root.right=root.left
+    root.left=temp
+    
     if(root.left)
         invertTree(root.left)
     if(root.right)
         invertTree(root.right)
     
-    let temp=root.right
-    root.right=root.left
-    root.left=temp
     return root
 };
