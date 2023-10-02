4a0264cebdd84cc14f58f027eb3abab9be60c9b8
Tadesse Dubale <tadalem26@gmail.com>
2023-10-02 15:34:54 +0300

    Time: 1075 ms (14.53%), Space: 232.3 MB (5.25%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.js
@@ -0,0 +1,87 @@
+
+var WordDictionary = function() {
+    this.tree={val: '', child: [], valid: false}
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.print = function(pre, root){
+    if(root.valid)
+        console.log(pre)
+    root.child.forEach(node=>{
+        this.print(pre+node.val, node)
+    })
+}
+
+WordDictionary.prototype.addWord = function(word) {
+    let myWords=word.split("")
+    function insert(root){
+        if(!myWords.length){
+            root.valid=true
+            return 
+        }
+        let newParent=null
+        let val=myWords.shift()
+        for(let node of root.child){
+            if(node.val==val){
+                newParent=node;
+                break;
+            }
+        }
+        if(!newParent){
+        newParent = {val: val, child: [], valid: false}
+        root.child.push(newParent)
+        }
+        insert(newParent)
+    }
+    insert(this.tree)
+    // console.log("printing", word)
+    // this.print("", this.tree)
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function(word) {
+    let searchWord=word.split("")
+    let found=false
+    
+    function matchChar(root, words){
+        if(found)
+            return
+        else if(!words.length){
+            if(root.valid)
+                found=true
+            return
+        }
+        let val=words.shift()
+        // if(val=='.'){
+        //     for(let node of root.child){
+        //       matchChar(node, words) 
+        //     }
+        // }else{
+        // let myWord=[..words]
+        for(let node of root.child){
+            if(val==node.val||val=='.'){
+                // if(word==".at")
+                //     console.log("before",words)
+              matchChar(node, [...words]) 
+                // if(word==".at")
+                //     console.log("after",words)
+                }
+            } 
+        // }
+    }
+    matchChar(this.tree, searchWord)
+    return found
+};
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
