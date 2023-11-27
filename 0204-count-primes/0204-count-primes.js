5055698a04a7f247bb9e8b553e56913ca5a2ddd5
Tadesse Dubale <tadessedev@gmail.com>
2023-11-27 22:57:59 +0300

    Time: 504 ms (19.41%), Space: 119.2 MB (42.89%) - LeetHub

--- 0204-count-primes/0204-count-primes.js
+++ 0204-count-primes/0204-count-primes.js
@@ -15,44 +15,5 @@ var countPrimes = function(n) {
         }
     }
     return arr.filter(ele=>ele).length-2
-//     if(n<2)
-//         return 0
-//     let res=0,i=2
-//     function isPrime(num){
-//         let j=2;
-//         let max=Math.ceil(Math.sqrr(num))
-//         while(j<=max){
-//             if(num%j==0)
-//                 return false
-//             j++
-//         }
-//         return true
-//     }
-    
-//     while(i<n){
-//         if(isPrime(i))
-//             res++
-//         i++
-//     }
-//     return res
-    
-    // function getThePrimes(n){
-    //     if(n<2)
-    //         return []
-    //     let primes=getThePrimes(n-1)
-    //     let i=0, isPrime=true, sqrt=Math.ceil(Math.sqrt(
-    //     while(i<primes.length&&isPrime){
-    //         if(n%primes[i]==0)
-    //             isPrime=false
-    //         if(primes[i]>sqrt)
-    //             break
-    //         i++
-    //     }
-    //     if(isPrime)
-    //         primes.push(n)
-    //     return primes
-    // }
-    // return getThePrimes(n).length
-    
-    
+
 };
