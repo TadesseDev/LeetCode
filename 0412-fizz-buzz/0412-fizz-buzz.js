dd446d7bf80ef86f585f6c9ed7912d2ab9cbc719
Tadesse Dubale <tadessedev@gmail.com>
2023-11-27 20:35:47 +0300

    Time: 60 ms (43.69%), Space: 44.3 MB (58.44%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let answer=[]
+    let start=1
+    while(start<=n){
+        if(start%3==0&&start%5==0)
+            answer.push("FizzBuzz")
+        else if(start%3==0)
+            answer.push("Fizz")
+        else if(start%5==0)
+            answer.push("Buzz")
+        else
+            answer.push(start.toString())
+        start++
+    }
+    return answer
+};
