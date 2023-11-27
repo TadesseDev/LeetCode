df558c023afc66118d36c3bfb04fd9a7fede4ec9
Tadesse Dubale <tadessedev@gmail.com>
2023-11-27 21:37:53 +0300

    Time: 61 ms (35.62%), Space: 42.9 MB (99.04%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
@@ -4,9 +4,18 @@
  * @return {number}
  */
 var numberOfEmployeesWhoMetTarget = function(hours, target) {
-    let sortedHour=hours.sort((a,b)=>a-b)
+    // let sortedHour=hours.sort((a,b)=>a-b)
+    // let i=0
+    // while(hours[i]<target)
+    //     i++
+    // return hours.length-i
+    
     let i=0
-    while(hours[i]<target)
+    let count=0
+    while(i<hours.length){
+        if(hours[i]>=target)
+            count++
         i++
-    return hours.length-i
+    }
+    return count
 };
