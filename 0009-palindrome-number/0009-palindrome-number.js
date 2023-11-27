c538663689ee473f75bc43c6cb6943565589d8c1
Tadesse Dubale <tadessedev@gmail.com>
2023-11-27 21:56:33 +0300

    Time: 123 ms (92.21%), Space: 50.6 MB (75.19%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,5 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-   return [...String(x)].reverse().join("")==String(x)
+   let num=[...String(x)]
+   let i=0,j=num.length-1
+   while(i<j){
+       if(num[i]!=num[j])
+           return false
+       i++
+       j--
+   }
+    return true
 };
