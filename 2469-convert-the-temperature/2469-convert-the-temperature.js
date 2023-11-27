4e8d516d907ef64142c4a62f0462cb6a2d840d3d
Tadesse Dubale <tadessedev@gmail.com>
2023-11-27 21:20:44 +0300

    Time: 54 ms (36.15%), Space: 42.1 MB (27.13%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -3,8 +3,5 @@
  * @return {number[]}
  */
 var convertTemperature = function(celsius) {
-    // let ans=celsius+273.15
-    // ans[1]=(ans[0]*1.80)+32.00
-    // console.log()
     return [celsius+273.15, celsius*1.80+32.00]
 };
