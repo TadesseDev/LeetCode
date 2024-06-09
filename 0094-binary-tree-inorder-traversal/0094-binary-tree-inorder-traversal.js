1fed27b1c40e97cf106fa497341ddb54fed88f39
Tadesse Dubale <tadessedev@gmail.com>
2024-06-09 21:35:16 +0300

    Time: 58 ms (21.71%), Space: 49.3 MB (14.94%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -11,24 +11,14 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    const result=[]
+    const vals = []
     if(!root)
-        return result
-    const stack=[root]
-    while(stack.length){
-        const current=stack.pop()
-        if(current.right){
-            stack.push(current.right)
-            current.right=null
-        }
-        
-        if(current.left){
-            stack.push(current)
-            stack.push(current.left)
-            current.left=null
-        }else {
-            result.push(current.val)
-        }
-    }
-    return result
+        return vals
+    if(root.left)
+        vals.push(...inorderTraversal(root.left))
+    // console.log(root.val)
+    vals.push(root.val)
+    if(root.right)
+        vals.push(...inorderTraversal(root.right))
+    return vals
 };
