9459a417c9f477dcfb8271ad0128ff3678f169bf
Tadesse Dubale <tadessedev@gmail.com>
2024-09-24 07:46:07 +0300

    Time: 57 ms (67.12%), Space: 50.9 MB (86.61%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -7,8 +7,3 @@ var missingNumber = function(nums) {
     const ourSum = nums.reduce((acc, val)=>acc+val, 0)
     return sum - ourSum 
 }
-    
-    /*
-    [0,1,2]
-    i=2, j=2, target=2, missing=-1
-    */
