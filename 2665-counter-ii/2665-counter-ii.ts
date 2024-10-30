81aa77b446e13f6730fbb25453221e5ac5f5ecf4
Tadesse Dubale <tadessedev@gmail.com>
2024-10-30 14:18:02 +0300

    Time: 63 ms (60.51%), Space: 52.6 MB (12.43%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.ts
@@ -0,0 +1,35 @@
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+    let i=init;
+    
+    function increment(){
+        i+=1
+        return i
+    }
+    function decrement(){
+        i-=1
+        return i
+    }
+    function reset(){
+        i=init;
+        return i
+    }
+    
+    return ({
+        increment,
+        decrement,
+        reset,
+    })
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
