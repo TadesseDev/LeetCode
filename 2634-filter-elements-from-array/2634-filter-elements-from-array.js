8d1840e66655fce4d67e9393948fa0df4840b367
Tadesse Dubale <tadessedev@gmail.com>
2024-11-01 23:24:00 +0300

    Time: 62 ms (10.94%), Space: 48.3 MB (95.10%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -6,8 +6,7 @@
 var filter = function(arr, fn) {
     const filterArray = [] 
     for(let i=0; i<arr.length; i++){
-        if(fn(arr[i], i))
-            filterArray.push(arr[i])
+            fn(arr[i], i) && filterArray.push(arr[i])
     }
     return filterArray
 };
