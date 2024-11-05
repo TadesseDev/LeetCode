e6e1d2a941ed7aadacad813ea24100b43463000c
Tadesse Dubale <tadessedev@gmail.com>
2024-11-05 20:23:42 +0300

    Time: 51 ms (65.08%), Space: 49.2 MB (5.27%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let count = 0
+    return function(...args){
+        count++;
+        if(count>1)
+            return 
+        return fn(...args)
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
