3e6ef7fbdf0ccdc6c2de24faeda62f108c8e7b0e
Tadesse Dubale <tadessedev@gmail.com>
2024-11-09 12:38:28 +0300

    Time: 60 ms (49.63%), Space: 49.3 MB (64.56%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timestamp = setTimeout(()=>{}, t)
+    return function(...args) {
+        clearTimeout(timestamp)
+        timestamp = setTimeout(()=>fn(...args), t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
