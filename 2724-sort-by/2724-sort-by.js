0549c35bb6954ad3d931387bae0f1f814fbc0c7f
Tadesse Dubale <tadessedev@gmail.com>
2024-11-11 09:28:06 +0300

    Time: 130 ms (41.73%), Space: 67.2 MB (12.53%) - LeetHub

--- 2724-sort-by/2724-sort-by.js
+++ 2724-sort-by/2724-sort-by.js
@@ -4,16 +4,5 @@
  * @return {Array}
  */
 var sortBy = function(arr, fn) {
-    const hash = {}
-    for(let i=0;i<arr.length;i++){
-        hash[fn(arr[i])] = arr[i]
-    }
-    
-    const sortedIndex = Object.keys(hash).sort((a,b)=>a-b)
-    const result=[]
-    for(let i=0; i<sortedIndex.length; i++){
-        result.push(hash[sortedIndex[i]])
-    }
-    
-    return result
+    return arr.sort((a,b)=>fn(a) - fn(b))
 };
