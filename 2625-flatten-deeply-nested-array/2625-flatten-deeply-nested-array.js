5b2fb4ad49bc2db4634d6124859d5e593aa3eaf9
Tadesse Dubale <tadessedev@gmail.com>
2024-11-12 16:42:14 +0300

    Time: 171 ms (28.02%), Space: 76.7 MB (68.32%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -5,18 +5,18 @@
  */
 var flat = function (arr, n) {
 
-    // function recurseFlate(arr, n){
+    function recurseFlate(arr, n){
         const result = []
         if(n==0)
             return arr
         for(let i=0; i<arr.length; i++){
             if(arr[i] instanceof Array && n>0)
-            result.push(...arguments.callee(arr[i], n-1))
+            result.push(...recurseFlate(arr[i], n-1))
             else
                 result.push(arr[i])
         }
         return result
-    // }
+    }
     
-    // return function.calle(arr, n)
+    return recurseFlate(arr, n)
 };
