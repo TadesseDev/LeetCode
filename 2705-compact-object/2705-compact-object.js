67ea72d753fb65815e5e937e5ba9180363d135d3
Tadesse Dubale <tadessedev@gmail.com>
2024-11-13 10:50:13 +0300

    Time: 84 ms (60.51%), Space: 63.5 MB (13.14%) - LeetHub

--- 2705-compact-object/2705-compact-object.js
+++ 2705-compact-object/2705-compact-object.js
@@ -9,13 +9,13 @@ var compactObject = function(obj) {
             return obj
         const result = obj instanceof Array ? [] : {}
 
-        const keys = Object.keys(obj)
-        keys.forEach((key)=>{
+        // const keys = Object.keys(obj)
+       for(let key in obj){
             if(obj[key]){
                 const response = compactMe(obj[key])
                result instanceof Array ? result.push(response) : result[key] = response
             }
-        })
+        }
         return result
     }
     return compactMe(obj)
