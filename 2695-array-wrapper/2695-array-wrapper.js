9b2fa69f3865e4d3ab187fe70731747d96ed0e48
Tadesse Dubale <tadessedev@gmail.com>
2024-11-15 02:24:33 +0300

    Time: 61 ms (14.78%), Space: 50.7 MB (25.19%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+        this.nums = nums
+    //    [Symbol.toPrimitive](hint){
+    //     if(hint=='number'){
+    //         return this.nums.reduce((accum, val)=>accum+val, 0)
+    //     }
+    //     else if(hint=='string'){
+    //         return `[${this.nums}]`
+    //     }
+    //     else 
+    //         return this.nums
+    // }
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    // [Symbol.toPrimitive](hint){
+    //     if(hint=='number'){
+            return this.nums.reduce((accum, val)=>accum+val, 0)
+        // }
+    // }
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    // [Symbol.toPrimitive](hint){
+    //     if(hint=='string'){
+            return `[${this.nums}]`
+        // }
+    // }
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
