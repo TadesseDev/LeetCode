b544004e7a7af661d29313c25ebbc5d94f67d482
Tadesse Dubale <tadessedev@gmail.com>
2025-01-03 20:22:10 +0300

    Time: 53 ms (77.81%), Space: 51.2 MB (50.2%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.ts
@@ -0,0 +1,16 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+        return ({
+            toBe: function(second: any){return val === second || (()=>{throw Error("Not Equal")})() },
+            notToBe: function(second: any){return val !== second || (()=>{throw  Error("Equal")})() }
+        })
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
