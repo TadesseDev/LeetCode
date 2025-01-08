bb0539f4b8941d582971986d33ba06b2c7c97918
Tadesse Dubale <tadessedev@gmail.com>
2025-01-08 12:53:41 +0300

    Time: 67 ms (11.49%), Space: 51.6 MB (27.75%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,21 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    
+    let called = 0 
+    return function (...args) {
+        if(called>0)
+        return 
+        called+=1
+        return fn(...args)
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
