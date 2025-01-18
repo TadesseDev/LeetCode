3a76fc21a9be5ef6fac830462d08cf1f09858d10
Tadesse Dubale <tadessedev@gmail.com>
2025-01-18 10:27:07 +0300

    Time: 3 ms (21.57%), Space: 51.5 MB (58.82%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.ts
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.ts
@@ -1,31 +1,7 @@
 function isRobotBounded(instructions: string): boolean {
-    /*
-    1. set all the possitions in order
-    2. set x and y values to 0 
-    3. set alterate to 1
-    4. set possition to 'Y'
-    4. start iteration 
-        5.1 if(G)
-            5.1.1 check if possition is Y or X
-            5.1.2 add alterate to x/y accoringly 
-        5.2 else(R/L)
-            5.2.1 if(R)
-                5.2.1.1 if(Y)
-                        possition=X
-                5.2.1.1 else
-                        possition=Y
-                        alterate*=-1
-            5.2.2 if(L)
-                5.2.1.1 if(Y)
-                        possition=X
-                        alterate*=-1
-                5.2.1.1 else
-                        possition=Y
-    6 if both x and y are 0 return true, if not return false 
-    */
+   
 
-    let x = 0, y = 0, alternate = 1, position = 'Y', results=[];
-    for (let i = 0; i <=instructions.length*instructions.length; i++) {
+    let x = 0, y = 0, alternate = 1, position = 'Y'
         Array.from(instructions).forEach(char => {
             if (char == 'G') {
                 if (position == 'Y')
@@ -50,20 +26,8 @@ function isRobotBounded(instructions: string): boolean {
                     position = 'Y'
             }
         })
-        if (results.includes([x,y].toString())){
-            results.push([x,y].toString())
-            break
-        }
-        results.push([x,y].toString())
-    }
 
-results.pop()
-    return results.includes([x,y].toString())
+console.log(alternate, position, x, y)
+    return alternate!==1 || position !== 'Y' || (x==0&&y==0)
 
-    //  const hash = Array.from(instructions).reduce((accum, val)=>{
-    //     accum[val] = accum[val] ? accum[val]+1 : 1
-    //     return accum
-    //  }, {});
-    // const value=hash[Object.keys(hash)[0]]
-    // return Object.keys(hash).every(result => value==result)
 };
