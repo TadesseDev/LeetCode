598e03040dbdcf0a757bc26c9d9c5ae653cb1052
Tadesse Dubale <tadessedev@gmail.com>
2025-01-23 13:56:21 +0300

    Time: 67 ms (7.2%), Space: 49.9 MB (91.65%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.ts
+++ 2619-array-prototype-last/2619-array-prototype-last.ts
@@ -3,7 +3,9 @@ interface Array<T> {
 }
 
 Array.prototype.last = function() {
-    return this[this.length-1] || -1
+    if(this.length)
+        return this[this.length-1] 
+    return -1
 };
 
 /**
