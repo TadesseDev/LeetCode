b4a557ca071c5c650a6b595c1ab9565c96804c89
Tadesse Dubale <tadessedev@gmail.com>
2025-11-04 20:56:53 +0400

    Time: 2 ms (42.06%), Space: 54 MB (80.34%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let j=0;
+    for(let i=0;i<t.length&&j<s.length;i++){
+        if(s[j]===t[i])
+            j++
+    }
+    return j==s.length
+};
