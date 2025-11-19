6c2ea320985266b564e5cc9f6c9b43dfa5f95a1c
Tadesse Dubale <tadessedev@gmail.com>
2025-11-19 09:51:36 +0400

    Time: 23 ms (66.37%), Space: 69.2 MB (58.33%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
@@ -5,8 +5,8 @@
 var removeStars = function(s) {
     let arr = []
     for(let i=0;i<s.length;i++){
-        if(s[i]!="*"){
-            arr.push(s[i])
+        if(s.charAt(i)!="*"){
+            arr.push(s.charAt(i))
         }else{
             arr.pop()
         }
