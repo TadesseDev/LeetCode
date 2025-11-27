b3de0280b493640664fb3f551122811c1458b5ce
Tadesse Dubale <tadessedev@gmail.com>
2025-11-27 19:45:54 +0400

    Time: 107 ms (5.08%), Space: 58.3 MB (61.49%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.js
+++ 0649-dota2-senate/0649-dota2-senate.js
@@ -11,39 +11,38 @@
 var predictPartyVictory = function(senate) {
     const head = new Node(senate[0]);
     let tail = head;
-    const map = new Map([["R", 0],["D", 0]]);
-    map.set(head.val, 1)
+   
+    
     for(let i=1; i<senate.length;i++){
         let node = new Node(senate[i]);
         tail.next=node;
         tail=tail.next;
-        map.set(node.val, map.get(node.val)+1)
+       
     }
     tail.next=head;
     tail=head;
 
     do{
         current = tail;
-        if(tail.val=="R" && map.get("D")){
+        if(tail.val=="R"){
             while(current.next.val == "R" && current.next!=tail){
                 current=current.next;
             }
             if(current.next==tail)
                 return "Radiant"
-                map.set("D", map.get("D")-1)
-        }else if(tail.val=="D" && map.get("R")){
+                
+        }else if(tail.val=="D"){
             while(current.next.val == "D" && current.next!=tail){
                 current=current.next;
             }
             if(current.next==tail)
                 return "Dire"
-                map.set("R", map.get("R")-1)
         }
 
         current.next=current.next.next;
         tail=tail.next;
         // console.log(map, map.get("D"), map.get("R"), tail.val, "\n")
-    }while(map.get("D")&&map.get("R"))
+    }while(true)
 
     return map.get("D") ? "Dire":"Radiant"
 };
