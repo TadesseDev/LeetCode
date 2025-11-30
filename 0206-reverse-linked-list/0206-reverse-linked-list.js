34eca248d4b5bbe7a00a6bf589703ad859298c2c
Tadesse Dubale <tadessedev@gmail.com>
2025-11-30 11:19:20 +0400

    Time: 0 ms (100%), Space: 56.8 MB (56.95%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,16 +10,20 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    
-    if(!head)
-        return head 
-    function reverse(node){
-        if(!node.next)
-            return [node, node]
-        let res=reverse(node.next)
-        node.next=null
-        res[0].next=node
-        return [node, res[1]]
-    }
-    return reverse(head)[1]
+
+    if(!head||!head.next)
+    return head
+
+        let head1=head
+        let head2=head.next;
+        head1.next=null;
+        do {
+            temp=head2.next
+            head2.next=head1;
+            head1=head2;
+            head2=temp;
+            // console.log(head1, head2)
+        }while(head2)
+
+        return head1;
 };
