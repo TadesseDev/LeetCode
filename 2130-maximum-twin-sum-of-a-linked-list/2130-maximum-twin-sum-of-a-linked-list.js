069d8b9bc086c788f282f8cfc8baedc7ea2f4ece
Tadesse Dubale <tadessedev@gmail.com>
2025-12-02 11:53:22 +0400

    Time: 2503 ms (5.17%), Space: 96 MB (5.17%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+
+/*
+* iterat to the last, keep counting as we call next node node. 
+* return last node as an array
+* keep unshifting and returning till array.length == current index; 
+* then keep suming and compairing to max sum
+* update max sum an unshift the element from the array
+* finally return max
+*/
+let max = 0;
+ function conpairTwins(current, index){
+    if(!current.next)
+        return [current]
+    const res = conpairTwins(current.next, index+1);
+    
+    if(res.length-1==index){
+        const twin=res.shift()
+        if(twin.val+current.val > max)
+            max=twin.val+current.val
+    }
+    else {
+        res.unshift(current)
+    }
+    return res;
+ }
+
+ conpairTwins(head, 0)
+ return max
+};
