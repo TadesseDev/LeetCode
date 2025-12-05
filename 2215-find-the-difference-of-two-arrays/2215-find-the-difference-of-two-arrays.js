dc817c2037bc47549343538710680ddc57ebff3d
Tadesse Dubale <tadessedev@gmail.com>
2025-12-05 16:24:41 +0400

    Time: 12 ms (35.37%), Space: 62.8 MB (24.61%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -7,16 +7,22 @@ var findDifference = function(nums1, nums2) {
     
     const hash1 = new Map(nums1.map(val=>[val, true]));
     const hash2 = new Map(nums2.map(val=>[val, true]));
-
-    nums1.forEach(val=>{
-        if(hash2.has(val)){
-            hash2.set(val, false)
-            hash1.set(val, false)
+const answer1=[]
+const answer2=[]
+    Array.from(hash1.keys()).forEach(val=>{
+        if(!hash2.has(val)){
+            answer1.push(val)
         }
     });
 
-    const aswer1 = Array.from(hash1.entries()).filter(item=>item[1]).map(item=>item[0])
-    const aswer2 = Array.from(hash2.entries()).filter(item=>item[1]).map(item=>item[0])
+    Array.from(hash2.keys()).forEach(val=>{
+        if(!hash1.has(val)){
+            answer2.push(val)
+        }
+    });
 
-    return [aswer1, aswer2]
+    // const aswer1 = Array.from(hash1.entries()).filter(item=>item[1]).map(item=>item[0])
+    // const aswer2 = Array.from(hash2.entries()).filter(item=>item[1]).map(item=>item[0])
+
+    return [answer1, answer2]
 };
