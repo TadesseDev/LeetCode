00dc3d3bf5a229845ab5357f5a6cded26e2362df
Tadesse Dubale <tadessedev@gmail.com>
2025-12-05 16:23:30 +0400

    Time: 16 ms (26.27%), Space: 62.9 MB (18.29%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    
+    const hash1 = new Map(nums1.map(val=>[val, true]));
+    const hash2 = new Map(nums2.map(val=>[val, true]));
+
+    nums1.forEach(val=>{
+        if(hash2.has(val)){
+            hash2.set(val, false)
+            hash1.set(val, false)
+        }
+    });
+
+    const aswer1 = Array.from(hash1.entries()).filter(item=>item[1]).map(item=>item[0])
+    const aswer2 = Array.from(hash2.entries()).filter(item=>item[1]).map(item=>item[0])
+
+    return [aswer1, aswer2]
+};
