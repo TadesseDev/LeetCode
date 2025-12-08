b6122584562feab2736b7ae327814fd24fa39c2e
Tadesse Dubale <tadessedev@gmail.com>
2025-12-08 22:24:03 +0400

    Time: 7 ms (90.34%), Space: 81.8 MB (7.14%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.js
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.js
@@ -32,22 +32,51 @@ left  1
     1: [3, 1]
     1: [2, 3]
   */  
-max=0;
-  function zigzag(node){
-    if(!node)
-        return [0,0]
+// max=0;
+//   function zigzag(node){
+//     if(!node)
+//         return [0,0]
     
-    let left = zigzag(node.right)[1]
-    let right = zigzag(node.left)[0]
+//     let right = zigzag(node.right)[1]
+//     let left = zigzag(node.left)[0]
 
-    max = Math.max(left, right, max)
-    return [left+1, right+1]
+//     max = Math.max(left, right, max)
+//     return [right+1, left+1]
+//   }
+
+
+//      let rest = zigzag(root)
+
+//     //  if(res[0]>max || res[1]>max)
+//     //     return Math
+//   return max;
+
+  /*
+    start by passing 0 for left and right on root
+        if left or right is greater than max, replas max 
+    - if node has left, call left with right +1
+    - if node has right, call it with left+1
+
+max=0
+1 0, 0 
+1 0, 1
+1 2, 0
+1 0, 3
+1 4, 0
+  */  
+
+let max=0;
+  function zigzag(node, left, right){
+
+    if(!node)
+        return;
+    max= Math.max(left, right, max)
+
+    zigzag(node.left, 0, left+1)
+    zigzag(node.right, right+1, 0)
+
   }
 
-
-     let rest = zigzag(root)
-
-    //  if(res[0]>max || res[1]>max)
-    //     return Math
-  return max;
+  zigzag(root, 0, 0)
+  return max
 };
