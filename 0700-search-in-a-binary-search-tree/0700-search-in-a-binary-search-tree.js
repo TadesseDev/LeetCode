c7cd5d65eb4896b38417bc2babfbf91744742aff
Tadesse Dubale <tadessedev@gmail.com>
2025-12-12 23:52:24 +0400

    Time: 1 ms (10.39%), Space: 63.2 MB (34.41%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    
+    if(!root || root.val==val)
+        return root;
+    
+    return searchBST(root.left, val) ?? searchBST(root.right, val);
+};
