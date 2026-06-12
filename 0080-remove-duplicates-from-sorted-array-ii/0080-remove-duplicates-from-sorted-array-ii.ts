09f505124fcd4e4cd5f23b9c592cde0f1cb9fd0d
Tadesse Dubale <tadessedev@gmail.com>
2026-06-12 16:31:37 +0400

    Time: 134 ms (7.83%), Space: 59.7 MB (48.6%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
@@ -1,14 +1,5 @@
 function removeDuplicates(nums: number[]): number {
 
-    // start from i=2
-    // on each iteration 
-    //   compair if i==i-2
-            // if true 
-                // start from k=i+1 and go till end or k != i
-                // remove all element i to k
-            // if not 
-            // continue 
-
         let i=2;
         while(i<nums.length){
 
@@ -16,11 +7,13 @@ function removeDuplicates(nums: number[]): number {
                 let k=i+1;
                 while(k<nums.length && nums[k]==nums[i])
                     k++;
-                
                 nums.splice(i, k-i)
+                i++
             }
             i++;
         }
+        // if(i==nums.length && nums[i-1]==nums[i-3])
+        //  nums.splice(i-1, 1)
         return nums.length;
     
 };
