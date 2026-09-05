2ce62b7d9cec941fed5bf7e0363a673edaf2838e
Tadesse Dubale <tadessedev@gmail.com>
2026-09-05 17:56:57 +0400

    Time: 2 ms (71.08%), Space: 67.2 MB (83.13%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.ts
+++ 0896-monotonic-array/0896-monotonic-array.ts
@@ -1,15 +1,15 @@
 function isMonotonic(nums: number[]): boolean {
     let increasing = nums[0] < nums[nums.length-1];
-    let stack=[nums[0]];
+    let prev=nums[0];
     for(let i=1;i<nums.length; i++){
         const val = nums[i];
 
-        if(increasing && val<stack[stack.length-1])
+        if(increasing && val<prev)
             return false
-        if(!increasing && val>stack[stack.length-1])
+        if(!increasing && val>prev)
             return false
+        prev = val
         
-        stack.push(val)
     }
     return true;
 };
