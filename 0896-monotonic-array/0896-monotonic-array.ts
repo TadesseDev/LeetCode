a0cdc42c0742ea653c020d3e3433265453703438
Tadesse Dubale <tadessedev@gmail.com>
2026-09-05 17:55:34 +0400

    Time: 3 ms (54.22%), Space: 70.8 MB (6.02%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.ts
+++ 0896-monotonic-array/0896-monotonic-array.ts
@@ -1,18 +1,15 @@
 function isMonotonic(nums: number[]): boolean {
-    // const newArr = nums.sort((a,b)=>a-b)
+    let increasing = nums[0] < nums[nums.length-1];
+    let stack=[nums[0]];
+    for(let i=1;i<nums.length; i++){
+        const val = nums[i];
 
-    let incre = true
-    let dec = true
-    for(let i=0;i<nums.length-1 && incre; i++){
-        if(nums[i]>nums[i+1] )
-            incre = false
+        if(increasing && val<stack[stack.length-1])
+            return false
+        if(!increasing && val>stack[stack.length-1])
+            return false
+        
+        stack.push(val)
     }
-
-if(incre)
-    return true 
-    for(let i=nums.length-1;i>0 && dec; i--){
-        if(nums[i-1]<nums[i] )
-            dec = false
-    }
-    return dec
+    return true;
 };
